3d138a406a33b1eeaad94629c6f6e7ba61318674
Seth Kerr <41877068+skerr92@users.noreply.github.com>
2019-05-29 13:06:09 -0400

    Update nodes-linkedlists-trees.java

--- Java/nodes-linkedlists-trees.java
+++ Java/nodes-linkedlists-trees.java
@@ -1,1 +1,43 @@
 // Java nodes and whatnot
+public static void main(String args[]) {
+  System.out.println("Hello World");
+}
+
+static class node {
+  private int data;
+  private node next;
+  node(int d) {
+    data = d;
+    next = null;
+  }
+  
+  node(int d, node p) {
+    data = d;
+    next = p;
+  }
+  
+  public void setNext(node p) {
+    next = p;
+  }
+  
+}
+
+static class LinkedList {
+  private node head;
+  private node tail;
+  
+  LinkedList(node p) {
+    head = p;
+    tail = null;
+  }
+  
+  LinkedList(node p, node r) {
+    head = p;
+    tail = r;
+  }
+  
+  public void insertAtHead(node p) {
+    p.setNext(head);
+  }
+ 
+}
